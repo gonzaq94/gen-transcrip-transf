203733861e6767d894d97fc90b22b423843c5bfa
Gonzalo Quintana <gonza.quintana94@gmail.com>
2025-01-11 16:03:51 +0100

    refactor dir

--- pipelines/gpt2.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-def call_gpt2(query: str) -> str:
-
-    generator = pipeline("text-generation", model="gpt2")
-    gen_text = generator(query, max_new_tokens=9000)
-
-    return gen_text[0]['generated_text'].lstrip(query)
